4b9c9c3832cacd0d7c5486a696c74537a45141ce
Kronberga, Kristine <kristine.kronberga@accenture.com>
2021-09-06 20:01:31 +0300

    Formatting 2

--- src/test/kotlin/api/tests/hotel_booking/tests/GetBookingByIdsTest.kt
+++ src/test/kotlin/api/tests/hotel_booking/tests/GetBookingByIdsTest.kt
@@ -1,6 +1,8 @@
 package api.tests.hotel_booking.tests
 
-import api.tests.hotel_booking.mapping.*
+import api.tests.hotel_booking.mapping.dateParser
+import api.tests.hotel_booking.mapping.getBookingJsonArray
+import api.tests.hotel_booking.mapping.getCreatedBookingJson
 import api.tests.hotel_booking.util.BookingRequests.createDefaultBooking
 import api.tests.hotel_booking.util.BookingRequests.defaultBooking
 import api.tests.hotel_booking.util.BookingRequests.deleteBookingWithCookieHeader
